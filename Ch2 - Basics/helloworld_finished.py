cced37e53bc3a0ddd987b460a08736d22f0a7348
alyhussein86 <111685594+alyhussein86@users.noreply.github.com>
2022-08-21 20:40:08 +0400

    Update helloworld_finished.py

--- Ch2 - Basics/helloworld_finished.py
+++ Ch2 - Basics/helloworld_finished.py
@@ -4,10 +4,32 @@
 #
 
 
-def main():
-    print("hello world!")
-    name = input("What is your name? ")
-    print("Nice to meet you,", name)
+# def main():
+#     print("hello world!")
+#     name = input("What is your name? ")
+#     print("Nice to meet you,", name)
+# if __name__ == "__main__":
+#     main()
 
-if __name__ == "__main__":
-    main()
+def testpali(mylist):
+    if mylist == mylist[::-1]:
+        return True
+    return False
+
+program = True
+
+while (program):
+    mylist = input("Enter Text for Palindrome Test or 'exit':")
+
+    mylist = mylist.lower()
+
+    if mylist == "exit":
+        program = False
+        break
+
+    newstr = ""
+    for x in mylist:
+        if x.isalnum():
+            newstr += x
+
+    print("The palindrome test is ",testpali(newstr))
